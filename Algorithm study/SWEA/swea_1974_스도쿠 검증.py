e5c5c09f3b4f44841db9d2dfacf017cc5ed359fd
dk <dgo0605@naver.com>
2021-08-20 12:59:38 +0900

    swea_1974 update

---
+++ Algorithm study/SWEA/swea_1974_스도쿠 검증.py
@@ -0,0 +1,50 @@
+# 행, 열, 사각형으로 순회하면서 기록하고.
+# 기록이 1이 아니면 스도쿠 아님 >> 0 반환
+def sudoku(arr):
+    # 퍼즐 크기
+    N = len(arr)
+
+    # 행 검사
+    for i in range(N):
+        # 확인한 숫자 기록
+        visited = [0] * (N + 1)
+        for j in range(N):
+            visited[arr[i][j]] += 1
+        # 1~9번 인덱스 값이 1이 아니면 0 반환
+        for d in range(1, N+1):
+            if visited[d] != 1:
+                return 0
+    # 열 검사
+    for i in range(N):
+        # 확인한 숫자 기록
+        visited = [0] * (N + 1)
+        for j in range(N):
+            visited[arr[j][i]] += 1
+        # 1~9번 인덱스 값이 1이 아니면 0 반환
+        for d in range(1, N+1):
+            if visited[d] != 1:
+                return 0
+
+    # 사각형 검사
+    for k in range(0, N, 3):
+        for m in range(0, N, 3):
+            # 확인한 숫자 기록
+            visited = [0] * (N + 1)
+            for i in range(k, k+3):
+                for j in range(m, m+3):
+                    visited[arr[i][j]] += 1
+            # 1~9번 인덱스 값이 1이 아니면 0 반환
+            for d in range(1, N+1):
+                if visited[d] != 1:
+                    return 0
+    # 모든 검사에서 이상 없으면 1 반환
+    return 1
+
+# 테스트케이스
+T = int(input())
+
+for tc in range(1, T+1):
+    # 스도쿠 퍼즐 입력
+    puzzle = [list(map(int, input().split())) for _ in range(9)]
+
+    print('#{} {}'.format(tc, sudoku(puzzle)))
