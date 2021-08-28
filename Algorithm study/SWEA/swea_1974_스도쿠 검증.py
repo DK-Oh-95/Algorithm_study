f9815771feef98050bdb3c3b2b620163d82fdc78
dk <dgo0605@naver.com>
2021-08-28 19:31:34 +0900

    swea_1974 modify

--- Algorithm study/SWEA/swea_1974_스도쿠 검증.py
+++ Algorithm study/SWEA/swea_1974_스도쿠 검증.py
@@ -2,43 +2,44 @@
 # 기록이 1이 아니면 스도쿠 아님 >> 0 반환
 def sudoku(arr):
     # 퍼즐 크기
-    N = len(arr)
+    n = len(arr)
 
     # 행 검사
-    for i in range(N):
+    for i in range(n):
         # 확인한 숫자 기록
-        visited = [0] * (N + 1)
-        for j in range(N):
+        visited = [0] * (n + 1)
+        for j in range(n):
             visited[arr[i][j]] += 1
         # 1~9번 인덱스 값이 1이 아니면 0 반환
-        for d in range(1, N+1):
+        for d in range(1, n+1):
             if visited[d] != 1:
                 return 0
     # 열 검사
-    for i in range(N):
+    for i in range(n):
         # 확인한 숫자 기록
-        visited = [0] * (N + 1)
-        for j in range(N):
+        visited = [0] * (n + 1)
+        for j in range(n):
             visited[arr[j][i]] += 1
         # 1~9번 인덱스 값이 1이 아니면 0 반환
-        for d in range(1, N+1):
+        for d in range(1, n+1):
             if visited[d] != 1:
                 return 0
 
     # 사각형 검사 (행렬을 3씩 구간을 나눠서 반복한다
-    for k in range(0, N, 3):
-        for m in range(0, N, 3):
+    for k in range(0, n, 3):
+        for m in range(0, n, 3):
             # 확인한 숫자 기록
-            visited = [0] * (N + 1)
+            visited = [0] * (n + 1)
             for i in range(k, k+3):
                 for j in range(m, m+3):
                     visited[arr[i][j]] += 1
             # 1~9번 인덱스 값이 1이 아니면 0 반환
-            for d in range(1, N+1):
+            for d in range(1, n+1):
                 if visited[d] != 1:
                     return 0
     # 모든 검사에서 이상 없으면 1 반환
     return 1
+
 
 # 테스트케이스
 T = int(input())
