3488bfcac153c43a35d241fa8a3eb7004c6398b3
dk <dgo0605@naver.com>
2022-01-23 23:35:07 +0900

    SOLVE: 22-01-23 programmers lv1 소수 만들기

--- Algorithm study/PROGRAMMERS/Lv. 1/소수 만들기.py
+++ Algorithm study/PROGRAMMERS/Lv. 1/소수 만들기.py
@@ -2,21 +2,31 @@ def solution(nums):
     answer = 0
     n = len(nums)
     used = [0] * n
-    power_set(used)
+    power_set(used, nums)
 
 
     return answer
 
-def power_set(arr):
+
+def power_set(arr, arr_nums):
     global answer
 
     if sum(arr) == 3:
+        tmp = 0
         for i in range(len(arr)):
+            if arr[i]:
+                tmp += arr_nums[i]
 
-            nums[i]
+        for i in range(2, tmp):
+            if tmp % i == 0:
+                break
+        else:
+            answer += 1
+
         return
 
-    for i in range(n):
-        arr[i] = 1
-        power_set(arr)
-        arr[i] = 0
+    for i in range(len(arr)):
+        if not arr[i]:
+            arr[i] = 1
+            power_set(arr)
+            arr[i] = 0
