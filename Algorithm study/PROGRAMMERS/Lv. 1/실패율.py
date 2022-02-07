3aea041444d4b9056cb68f837079b2857878d187
dk <dgo0605@naver.com>
2022-02-07 23:54:28 +0900

    SOLVE: 22-02-07 programmers lv1 실패율

--- Algorithm study/PROGRAMMERS/Lv. 1/실패율.py
+++ Algorithm study/PROGRAMMERS/Lv. 1/실패율.py
@@ -1,21 +1,5 @@
 def solution(N, stages):
     answer = []
-    tmp_list = []
-    tmp_num = 0
-    n = len(stages)
-    for i in range(1, N+1):
-        a = stages.count(i)
-        tmp_list.append(a/(n-tmp_num))
-        tmp_num += a
-    tmp_list2 = sorted(tmp_list)
-    tmp_list2.reverse()
-    used = [0] * N
-
-    for i in range(N):
-        for j in range(N):
-            if not used[j] and tmp_list2[i] == tmp_list[j]:
-                used[j] = 1
-                answer.append(j+1)
 
     return answer
 
